07984246a700e5b1500d49c688c2c9ad29ffbb3b
CynaraCosta <cvocac@cin.ufpe.br>
2022-09-12 08:47:41 -0300

    first first commit

---
+++ Organizart/Organizart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Organizart
+//
+//  Created by Cynara Costa on 12/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
