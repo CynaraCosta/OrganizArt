541e9da1c2edf1cbb404eb2146ba094265f40dfc
CynaraCosta <cvocac@cin.ufpe.br>
2022-09-15 11:35:04 -0300

    fix(feature/tab-bar-fill): fixed to selected image be fill at the tab bar

--- Organizart/Organizart/Controllers/ViewController.swift
+++ Organizart/Organizart/Controllers/ViewController.swift
@@ -12,21 +12,23 @@ class ViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tabBar.backgroundColor = .systemBackground
-        let Images = ["house", "bag", "shippingbox"]
         
         let homeVC = HomeViewController()
         homeVC.title = "Início"
+        homeVC.tabBarItem.image = UIImage(systemName: "house")
+        homeVC.tabBarItem.selectedImage = UIImage(systemName: "house.fill")
+        
         let salesVC = SalesViewController()
         salesVC.title = "Vendas"
+        salesVC.tabBarItem.image = UIImage(systemName: "bag")
+        salesVC.tabBarItem.selectedImage = UIImage(systemName: "bag.fill")
+        
         let productsVC = ProductsViewController()
         productsVC.title = "Produtos"
+        productsVC.tabBarItem.image = UIImage(systemName: "shippingbox")
+        productsVC.tabBarItem.selectedImage = UIImage(systemName: "shippingbox.fill")
         
         self.setViewControllers([homeVC, salesVC, productsVC], animated: false)
-        guard let items = self.tabBar.items else {return}
-        
-        for vc in (0...Images.count - 1) {
-            items[vc].image = UIImage(systemName: Images[vc])
-        }
         
         self.tabBar.tintColor = UIColor(named: "purple-700")
         self.tabBar.unselectedItemTintColor = UIColor(named: "purple-500")
